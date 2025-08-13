8c096d66d15e1f214af23db8a88e8d97ba28ae99
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-08-13 11:52:02 -0500

    Update

--- app/(routes)/dashboard/_components/SideNav.jsx
+++ app/(routes)/dashboard/_components/SideNav.jsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { LayoutGrid, PiggyBank, ReceiptText, ShieldCheck } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 function SideNav() {
   const menuList = [
@@ -9,41 +13,53 @@ function SideNav() {
       id: 1,
       name: "Dashboard",
       icon: LayoutGrid,
-      path: '',
+      path: "/dashboard",
     },
     {
       id: 2,
       name: "Budgets",
       icon: PiggyBank,
-      path: '',
+      path: "/dashboard/budgets",
     },
     {
       id: 3,
       name: "Expenses",
       icon: ReceiptText,
-      path: '',
+      path: "/dashboard/expenses",
     },
     {
       id: 4,
       name: "Upgrade",
       icon: ShieldCheck,
-      path: '',
+      path: "/dashboard/upgrade",
     },
   ];
+
+  const path = usePathname();
+
+  useEffect(() => {
+    console.log(path);
+  }, []);
 
   return (
     <div className="h-screen p-5 border shadow-md">
       <Image src={"/logo.svg"} alt="PennyPilot Logo" width={50} height={50} />
       <div className="mt-5">
         {menuList.map((menu, idx) => (
-            <h2 className="flex gap-2 items-center text-gray-700 font-medium p-5 cursor-pointer rounded-md hover:text-indigo-700 hover:bg-blue-100 hover:shadow-md transition-all duration-400">
-                <menu.icon/>
-                {menu.name}
+          <Link href={menu.path}>
+            <h2
+              className={`flex gap-2 items-center text-gray-700 font-medium mb-3 p-5 cursor-pointer rounded-md hover:text-indigo-700 hover:bg-blue-100 hover:shadow-md transition-all duration-400 ${
+                path == menu.path && "text-indigo-700 bg-blue-100"
+              }`}
+            >
+              <menu.icon />
+              {menu.name}
             </h2>
+          </Link>
         ))}
       </div>
       <div className="fixed bottom-10 p-5 flex gap-2 items-center">
-        <UserButton/>
+        <UserButton />
         Profile
       </div>
     </div>
