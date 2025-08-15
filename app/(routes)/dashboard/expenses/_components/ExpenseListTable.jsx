f1bd74a46a9fe94ec228e28b74248c8cae6519ef
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-08-15 13:21:51 -0500

    Added alert dialog for expense deletion

--- app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
+++ app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
@@ -5,6 +5,18 @@ import { Trash2 } from "lucide-react";
 import React from "react";
 import { toast } from "sonner";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
 function ExpenseListTable({ expensesList, refreshData }) {
   const deleteExpense = async (expense) => {
     const result = await db
@@ -12,11 +24,10 @@ function ExpenseListTable({ expensesList, refreshData }) {
       .where(eq(Expenses.id, expense.id))
       .returning();
 
-      if(result)
-      {
-        toast('Expense Deleted!');
-        refreshData()
-      }
+    if (result) {
+      toast("Expense Deleted!");
+      refreshData();
+    }
   };
 
   return (
@@ -28,17 +39,33 @@ function ExpenseListTable({ expensesList, refreshData }) {
         <h2>Action</h2>
       </div>
       {expensesList.map((expenses, idx) => (
-        <div key={idx} className="mt-4 grid grid-cols-4 bg-slate-50 p-4 rounded-lg hover:bg-slate-100 transform transition-all duration-400 hover:scale-102 ease-out hover:shadow-md">
+        <div
+          key={idx}
+          className="mt-4 grid grid-cols-4 bg-slate-50 p-4 rounded-lg hover:bg-slate-100 transform transition-all duration-400 hover:scale-102 ease-out hover:shadow-md"
+        >
           <h2>{expenses.name}</h2>
-          <h2 className="text-indigo-600 font-bold">
-            ${expenses.amount}
-          </h2>
+          <h2 className="text-indigo-600 font-bold">${expenses.amount}</h2>
           <h2>{expenses.createdAt}</h2>
           <h2 className="ml-3 cursor-pointer">
-            <Trash2
-              className="text-red-600"
-              onClick={() => deleteExpense(expenses)}
-            />
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <Trash2
+                  className="text-red-600"
+                />
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you sure you want to delete the expense?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action cannot be undone. This will permanently delete the expense from the server.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogAction className="bg-red-600" onClick={() => deleteExpense(expenses)}>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
           </h2>
         </div>
       ))}
