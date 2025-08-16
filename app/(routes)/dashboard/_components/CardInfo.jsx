e065af5b74214c3501993965a634fd4817f3556f
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-08-16 18:38:25 -0500

    Added code for Expenses screen

--- app/(routes)/dashboard/_components/CardInfo.jsx
+++ app/(routes)/dashboard/_components/CardInfo.jsx
@@ -29,7 +29,7 @@ function CardInfo({ budgetList }) {
         <div className="mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <div className="p-7 border rounded-lg flex items-center justify-between border-2 shadow-md shadow-indigo-300 transform transition-all duration-400 hover:scale-104 hover:shadow-lg ease-out cursor-pointer hover:border-indigo-100 hover:shadow-indigo-300">
             <div>
-              <h2 className="text-sm">Total Budget Allocated</h2>
+              <h2 className="text-sm">Total Budget</h2>
               <h2 className="mt-1 font-bold text-xl text-green-600">
                 +${totalBudget}
               </h2>
@@ -38,7 +38,7 @@ function CardInfo({ budgetList }) {
           </div>
           <div className="p-7 border rounded-lg flex items-center justify-between border-2 shadow-md shadow-indigo-300 transform transition-all duration-400 hover:scale-104 hover:shadow-lg ease-out cursor-pointer hover:border-indigo-100 hover:shadow-indigo-300">
             <div>
-              <h2 className="text-sm">Total Amount Spent</h2>
+              <h2 className="text-sm">Total Spent</h2>
               <h2 className="mt-1 font-bold text-xl text-red-600">
                 -${totalSpent}
               </h2>
