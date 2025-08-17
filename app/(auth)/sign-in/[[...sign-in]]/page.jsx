df9438c5052fe9604b68a48ff4c5a7b789cb07ce
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-08-17 13:40:18 -0500

    Changed messages in Sign-in, Sign-up pages

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -32,8 +32,7 @@ export default function Page() {
             </h2>
 
             <p className="mt-4 leading-relaxed text-white/90">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-              nam dolorum aliquam, quibusdam aperiam voluptatum.
+              Track every penny, set smarter budgets, and make confident money decisions — all in one simple, intuitive app.
             </p>
           </div>
         </section>
@@ -64,8 +63,7 @@ export default function Page() {
               </h1>
 
               <p className="mt-4 leading-relaxed text-gray-500">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Eligendi nam dolorum aliquam, quibusdam aperiam voluptatum.
+                Track every penny, set smarter budgets, and make confident money decisions — all in one simple, intuitive app.
               </p>
             </div>
 
