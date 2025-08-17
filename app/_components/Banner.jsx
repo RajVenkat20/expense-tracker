11648e895a2860d7753868d9ed1699d9e8c10e41
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-08-17 13:26:40 -0500

    Rounded Get Started button

--- app/_components/Banner.jsx
+++ app/_components/Banner.jsx
@@ -16,7 +16,7 @@ function Banner() {
 
           <div className="mt-4 flex justify-center gap-4 sm:mt-6">
             <a
-              className="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-md transition-colors hover:bg-indigo-700"
+              className="inline-block rounded-lg border bg-indigo-600 px-5 py-3 font-medium text-white shadow-lg transition-all transform ease-out duration-400 hover:scale-110 hover:bg-indigo-700"
               href="/sign-in"
             >
               Get Started
