15e823b4724f2487a81ad9f2adcdfa53fd18129e
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-08-17 21:01:46 -0500

    Added code for appropriate greetings based on time

--- app/(routes)/dashboard/_components/DashboardHeader.jsx
+++ app/(routes)/dashboard/_components/DashboardHeader.jsx
@@ -1,17 +1,31 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+import { UserButton, useUser } from "@clerk/nextjs";
+import React, { useMemo } from "react";
 
 function DashboardHeader() {
+  const {user} = useUser();
+
+  const greeting = useMemo(() => {
+    const h = new Date().getHours();
+    if(h < 12) return "Good morning";
+    if(h < 18) return "Good afternoon"
+    return "Good evening"
+  }, []);
+
   return (
-    <div className='p-5 shadow-md border-bottom flex justify-between'>
-        <div>
-            {/* Content to be added later according to need */}
-        </div>
-        <div>
-            <UserButton/>
-        </div>
+    <div className="p-5 shadow-md items-center border-bottom flex justify-between bg-blue-100">
+      <div>
+        <h2 className="font-bold text-3xl">
+          {greeting}, <span className="text-indigo-600">{user?.firstName}!</span> ✌️
+        </h2>
+        <p className="text-gray-500 mt-2">
+          Here's what's happening with your money!
+        </p>
+      </div>
+      <div>
+        <UserButton />
+      </div>
     </div>
-  )
+  );
 }
 
-export default DashboardHeader
+export default DashboardHeader;
