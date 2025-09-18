9f891f182fa41db645f0218737d8e7193d518955
Raj Venkat Reddy Mavuram <mrajvenkatreddy@gmail.com>
2025-09-18 14:00:41 -0500

    Added a new option in the Side Nav for future implementation

--- app/(routes)/dashboard/_components/SideNav.jsx
+++ app/(routes)/dashboard/_components/SideNav.jsx
@@ -26,6 +26,7 @@ function SideNav({
     { id: 1, name: "Dashboard", icon: LayoutGrid, path: "/dashboard" },
     { id: 2, name: "Budgets", icon: PiggyBank, path: "/dashboard/budgets" },
     { id: 3, name: "Expenses", icon: ReceiptText, path: "/dashboard/expenses" },
+    { id: 4, name: "Reports", icon: ReceiptText, path: "/dashboard/expenses" },
   ];
 
   const path = usePathname();
